e0a41f569a810e50d971fcdf1853d4a95c067690
DaviMaroto <davimaroto09@gmail.com>
2024-02-07 23:10:17 -0300

    Realizando tratamento de erros

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-document.querySelector("form").addEventListener("submit", evert =>  {
+document.querySelector("form").addEventListener("submit", evert => {
     console.log("Enviar o formulário")
     //Não envia o formulário
     event.preventDefault()
@@ -7,21 +7,33 @@ document.querySelector("form").addEventListener("submit", evert =>  {
 const field = document.querySelectorAll("[required]") //pega todos os inputs que estão com o campo required
 console.log(field)
 
-function customValidation(event){
+function customValidation(event) {
     const fields = event.target
 
     //Verificar se existem erros
-    function verifyError(){
+    function verifyError() {
         let foundError = false
 
-        for(error in fields.validity)
-        console.log(error)
-        // return foundError
+        for (const error in fields.validity) {
+            // se não tiver customError
+            // Então verifica se tem erro 
+            if (error != "customError" && fields.validity[error]) {
+                foundError = error
+            }
+        }
+        return foundError
     }
-verifyError()
-    //Troca mensagem de required
-    fields.setCustomValidity("Esse campo é obrigatório")
+    const error = verifyError()
+    console.log("Error Exists", error)
+
+    if (error) {
+        fields.setCustomValidity("Esse campo é obrigatório") //Troca mensagem de required
+    } else {
+        fields.setCustomValidity("")
+    }
+
 }
-for (fields of field){ //atribui um inut a variavel fields
+
+for (fields of field) { //atribui um input a variavel fields
     fields.addEventListener("invalid", customValidation) //evento invalido
 }
